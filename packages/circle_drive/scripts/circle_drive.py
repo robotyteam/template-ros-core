8006945ee712a6871f2e7ed1923b5eb2d4c6434c
Konstantin Chaika <pro100kot14@gmail.com>
2021-04-30 18:27:52 +0300

    add stop line reading example

--- packages/circle_drive/scripts/circle_drive.py
+++ packages/circle_drive/scripts/circle_drive.py
@@ -2,18 +2,25 @@
 import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
-from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import Twist2DStamped, StopLineReading
 
 class MyNode(DTROS):
 
     def __init__(self, node_name):
         super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
         self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
+        self.sub_stop_line = rospy.Subscriber("stop_line_filter_node/stop_line_reading", StopLineReading, self.cb_stop_line)
+
+    def cb_stop_line(self, msg):
+        print("cb_stop_line():")
+        print(msg.stop_line_detected)
+        print(msg.stop_line_point.x)
 
     def run(self):
         # publish message every 1 second
         rate = rospy.Rate(0.5) # 1Hz
         while not rospy.is_shutdown():
+            continue
             msg = Twist2DStamped()
             msg.v = 0.0
             msg.omega = 1.0
