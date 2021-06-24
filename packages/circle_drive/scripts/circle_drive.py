d631e9160b6c1b1ebf84b0234c316584f32c42bf
Konstantin Chaika <pro100kot14@gmail.com>
2021-06-24 12:57:20 +0300

    zoom call codeexperiments

--- packages/circle_drive/scripts/circle_drive.py
+++ packages/circle_drive/scripts/circle_drive.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import rospy
+from std_msgs.msg import Bool
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import Twist2DStamped, StopLineReading
 
@@ -9,26 +10,46 @@ class MyNode(DTROS):
     def __init__(self, node_name):
         super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
         self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
-        self.sub_stop_line = rospy.Subscriber("stop_line_filter_node/stop_line_reading", StopLineReading, self.cb_stop_line)
+        self.sub_fw = rospy.Subscriber("go_forward", Bool, self.cb_fw)
+        self.sub_bw = rospy.Subscriber("go_backward", Bool, self.cb_bw)
 
-    def cb_stop_line(self, msg):
-        print("cb_stop_line():")
-        print(msg.stop_line_detected)
-        print(msg.stop_line_point.x)
+
+
+    def cb_fw(self, msg):
+        rospy.loginfo("cd_fw")
+        rate = rospy.Rate(0.5)
+        msg = Twist2DStamped()
+        msg.v = 0.5
+        msg.omega = 0.0
+        rospy.loginfo("Publishing message")
+        self.pub.publish(msg)
+        rate.sleep()
+        msg.v = 0.0
+        rospy.loginfo("Publishing message")
+        self.pub.publish(msg)
+    
+    def cb_bw(self, msg):
+        rospy.loginfo("cd_bw")
+        rate = rospy.Rate(0.5)
+        msg = Twist2DStamped()
+        msg.v = -0.5
+        msg.omega = 0.0
+        rospy.loginfo("Publishing message")
+        self.pub.publish(msg)
+        rate.sleep()
+        msg.v = 0.0
+        rospy.loginfo("Publishing message")
+        self.pub.publish(msg)
+
+
 
     def run(self):
         # publish message every 1 second
         rate = rospy.Rate(0.5) # 1Hz
         while not rospy.is_shutdown():
-            msg = Twist2DStamped()
-            msg.v = 0.0
-            msg.omega = 1.0
-            rospy.loginfo("Publishing message")
-            self.pub.publish(msg)
+            self.cb_bw(None)
             rate.sleep()
-            msg.omega = 0.0
-            rospy.loginfo("Publishing message -")
-            self.pub.publish(msg)
+            self.cb_fw(None)
             rate.sleep()
 
 if __name__ == '__main__':
